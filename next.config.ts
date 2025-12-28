16d99702d768ff957598ef19a2ba0c4bec589e9c
serahan <seyrahan22@gmail.com>
2025-12-28 14:30:53 +0900

    feat: CesiumJS 설치 및 webpack 설정 추가
    #1

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,34 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    reactCompiler: true,
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.alias = {
+                ...config.resolve.alias,
+                cesium: "cesium/Build/Cesium/Cesium.js",
+            };
+
+            config.module.rules.push({
+                test: /\.js$/,
+                include: /cesium/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                },
+            });
+        }
+
+        // 정적 파일 처리
+        config.module.rules.push({
+            test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
+            use: ["url-loader"],
+        });
+
+        return config;
+    },
 };
 
 export default nextConfig;
