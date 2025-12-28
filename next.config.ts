0154325aa8d7dc65a254493630e9d7e854aa0693
serahan <seyrahan22@gmail.com>
2025-12-28 16:45:42 +0900

    feat: CesiumJS 로드
    #1

--- next.config.ts
+++ next.config.ts
@@ -2,33 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     reactCompiler: true,
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.resolve.alias = {
-                ...config.resolve.alias,
-                cesium: "cesium/Build/Cesium/Cesium.js",
-            };
-
-            config.module.rules.push({
-                test: /\.js$/,
-                include: /cesium/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env"],
-                    },
-                },
-            });
-        }
-
-        // 정적 파일 처리
-        config.module.rules.push({
-            test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
-            use: ["url-loader"],
-        });
-
-        return config;
-    },
 };
 
 export default nextConfig;
